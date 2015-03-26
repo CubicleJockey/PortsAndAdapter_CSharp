c6ec6ff1381567af7fc0a19f907d0181891d94e9
André Davis <cubicle.jockey@live.com>
2015-03-26 16:12:54 -0700

    1. Adding more unit tests
    Heading off to gym weeee

---
+++ Src/PortsAndAdapters.Core.Tests.Unit/UseCases/Shared/Reads/GetAllSamplesUseCaseTests.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PortsAndAdapters.Core.Ports.Secondary.StorageEngine.Queriers;
+using PortsAndAdapters.Core.UseCaseInputs;
+using PortsAndAdapters.Core.UseCases.Shared.Reads;
+using PortsAndAdapters.Core.UtilityTypes;
+using PortsAndAdapters.Core.Views;
+
+namespace PortsAndAdapters.Core.Tests.Unit.UseCases.Shared.Reads
+{
+    public class GetAllSamplesUseCaseTests
+    {
+        [TestClass]
+        public class Constructors
+        {
+            [TestMethod]
+            public void Valid()
+            {
+                #region Arrange
+
+                var fakeSampleQuerier = A.Fake<ISampleQuerier>();
+
+                #endregion Arrange
+
+                #region Act
+
+                var usecase = new GetAllSamplesUseCase(fakeSampleQuerier);
+
+                #endregion Act
+
+                #region Assert
+
+                usecase.Should().NotBeNull();
+                usecase.Should().BeAssignableTo<IUseCase<IEmptyInput, IEnumerable<ISampleView>>>();
+                usecase.Should().BeOfType<GetAllSamplesUseCase>();
+
+                #endregion Assert
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentNullException))]
+            public void Invalid()
+            {
+                #region Arrange
+
+                ISampleQuerier sampleQuerier = null;
+
+                #endregion Arrange
+
+                #region Act/Assert
+
+                var usecase = new GetAllSamplesUseCase(sampleQuerier);
+
+                #endregion Act/Assert
+            }
+        }
+
+        [TestClass]
+        public class Methods
+        {
+            [TestMethod]
+            public void Execute()
+            {
+                #region Arrange
+
+                IEnumerable<ISampleView> samplesValue = new[]
+                {
+                    new SampleView(Guid.NewGuid(), "Batman", "DarkNight"),
+                    new SampleView(Guid.NewGuid(), "Optimus Rhyme", "NerdCore") 
+                };
+
+                var sampleQuerier = A.Fake<ISampleQuerier>();
+                A.CallTo(() => sampleQuerier.GetAll()).Returns(samplesValue);
+
+                var usecase = new GetAllSamplesUseCase(sampleQuerier);
+                usecase.Should().NotBeNull();
+
+                #endregion Arrange
+
+                #region Act
+
+                var result = usecase.Execute(new EmptyInput()).ToArray();
+
+                #endregion Act
+
+                #region Assert
+
+                result.Should().NotBeNullOrEmpty();
+                result.Length.ShouldBeEquivalentTo(samplesValue.Count());
+
+                for (var i = 0; i < result.Length; i++)
+                {
+                    result.Contains(samplesValue.ElementAt(i)).Should().BeTrue();
+                }
+
+                #endregion Assert
+            }
+        }
+    }
+}
---
+++ Src/PortsAndAdapters.Core.Tests.Unit/UseCases/Shared/Reads/GetSampleByIdUseCaseTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PortsAndAdapters.Core.Ports.Secondary.StorageEngine.Queriers;
+using PortsAndAdapters.Core.UseCaseInputs.Shared.Reads;
+using PortsAndAdapters.Core.UseCases.Shared.Reads;
+using PortsAndAdapters.Core.UtilityTypes;
+using PortsAndAdapters.Core.Views;
+
+namespace PortsAndAdapters.Core.Tests.Unit.UseCases.Shared.Reads
+{
+    public class GetSampleByIdUseCaseTests
+    {
+        [TestClass]
+        public class Constructors
+        {
+            [TestMethod]
+            public void Valid()
+            {
+                #region Arrange
+
+                var querier = A.Fake<ISampleQuerier>();
+
+                #endregion Arrange
+
+                #region Act
+
+                var usecase = new GetSampleByIdUseCase(querier);
+
+                #endregion Act
+
+                #region Assert
+
+                usecase.Should().NotBeNull();
+                usecase.Should().BeAssignableTo<IUseCase<ISampleIdInputs, ISampleView>>();
+                usecase.Should().BeOfType<GetSampleByIdUseCase>();
+
+                #endregion Assert
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(ArgumentNullException))]
+            public void Invalid()
+            {
+                #region Arrange
+
+                ISampleQuerier querier = null;
+
+                #endregion Arrange
+
+                #region Act/Assert
+
+                var usecase = new GetSampleByIdUseCase(querier);
+
+                #endregion Act/Assert
+            }
+        }
+
+        [TestClass]
+        public class Methods
+        {
+            [TestMethod]
+            public void Execute()
+            {
+                #region Arrange
+
+                var id = Guid.NewGuid();
+                var findId = new SampleIdInputs(id);
+
+                var foundSample = new SampleView(id, "Kellee Maize", "New Age");
+                
+                var querier = A.Fake<ISampleQuerier>();
+                A.CallTo(() => querier.GetWithId(findId)).Returns(foundSample);
+                
+                #endregion Arrange
+            }
+        }
+    }
+}
